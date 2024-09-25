c5c6f401940589392674f4a4b8fb7c8bc6ead4f6
moskvichev evgeniy <moskvichev_e@bk.ru>
2024-09-25 23:46:31 +0400

    add to cart

--- public/js/cart.js
+++ public/js/cart.js
@@ -24,8 +24,25 @@ function ajaxGetGoodsInfo() {
       'Content-Type': 'application/json',
     },
   })
-    .then(function (response) {})
+    .then(function (response) {
+      return response.text();
+    })
     .then(function (body) {
       console.log(body);
+      showCart(JSON.parse(body));
     });
+}
+
+function showCart(data) {
+  let out = '<table class="table table-striped table-cart"><tbody>';
+  let total = 0;
+  for (let key in cart) {
+    out += `<tr><td><a href="/goods?id=${key}">${data[key]['name']}</a></tr>`;
+    out += `<tr><td><i class="far fa-minus-square cart-minus" data-goods_id="${key}"></i><td>`;
+    out += `<td>${cart[key]}<td>`;
+    out += `<td><i class="far fa-plus-square cart-plus" data-goods_id="${key}"></i><td>`;
+    out += `</tr>`;
+  }
+  out += '</tbody></table>';
+  document.querySelector('#cart-nav').innerHTML = out;
 }
