9b97136b6844f15d9123c8a028b41767586cada2
moskvichev evgeniy <moskvichev_e@bk.ru>
2024-10-09 00:25:46 +0400

    node mailer

--- public/js/order.js
+++ public/js/order.js
@@ -7,10 +7,24 @@ document.querySelector('#lite-shop-order').onsubmit = function (event) {
 
   if (!document.querySelector('#rule').checked) {
     //с правилами не согласен
+    Swal.fire({
+      title: 'Error!',
+      text: 'Do you want to continue',
+      icon: 'error',
+      confirmButtonText: 'Cool',
+    });
+    return false;
   }
 
   if (username == '' || phone == '' || email == '' || address == '') {
     // не заполнены поля
+    Swal.fire({
+      title: 'Warning',
+      text: 'Fill all fields',
+      icon: 'info',
+      confirmButtonText: 'Ok',
+    });
+    return false;
   }
 
   fetch('/finish-order', {
@@ -32,7 +46,19 @@ document.querySelector('#lite-shop-order').onsubmit = function (event) {
     })
     .then(function (body) {
       if (body == 1) {
+        Swal.fire({
+          title: 'Success',
+          text: 'Success',
+          type: 'info',
+          confirmButtonText: 'Ok',
+        });
       } else {
+        Swal.fire({
+          title: 'Problem with mail',
+          text: 'Error',
+          type: 'Error',
+          confirmButtonText: 'Ok',
+        });
       }
     });
 };
